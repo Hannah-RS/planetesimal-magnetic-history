13b6a4f708ec0f1b860e7dcffb3b8a34b0636c6f
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2022-12-20 14:03:28 +0000

    Added a 3rd model

--- Version4/plot_compare_runs.py
+++ Version4/plot_compare_runs.py
@@ -10,15 +10,22 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 #choose your runs
-run1 = 34 #38
-run2 = 35 #39
-
+run1 = 62
+run2 = 64 
+run3 = 65
+#choose model labels
+model1 = 'Bryson et. al. (2019)'
+model2 = 'Bryson $\phi$=0.3'
+model3 = 'Bryson $\eta_0$=10$^{14}$Pas'
+# model1= 'Dodds et. al. (2021)'
+# model2 = 'Bryson et al. (2019)'
+# model3='Sterenborg & Crowley (2013)'
 #scale time to Myr
-from parameters import Myr, r, Tm0, Tsolidus
+from parameters import Myr, r, Tm0
 
 #import data from npz file - run1
 npzfile = np.load('Results/run_{}.npz'.format(run1))
-Tm1 = npzfile['Tm_base'] 
+Tm1 = npzfile['Tm_mid'] 
 Tm_surf1 = npzfile['Tm_surf'] 
 Tc1= npzfile['Tc'] 
 t1 = npzfile['t'] #time in s
@@ -35,7 +42,7 @@ t_plot1 = t1/Myr
 
 #import data from npz file - run2
 npzfile = np.load('Results/run_{}.npz'.format(run2))
-Tm2 = npzfile['Tm_base'] 
+Tm2 = npzfile['Tm_mid'] 
 Tm_surf2 = npzfile['Tm_surf'] 
 Tc2= npzfile['Tc'] 
 t2 = npzfile['t'] #time in s
@@ -50,8 +57,25 @@ Frad2 = Flux2[3]
 
 t_plot2 = t2/Myr
 
+#import data from npz file - run3
+npzfile = np.load('Results/run_{}.npz'.format(run3))
+Tm3 = npzfile['Tm_mid'] 
+Tm_surf3 = npzfile['Tm_surf'] 
+Tc3= npzfile['Tc'] 
+t3 = npzfile['t'] #time in s
+Flux3 = npzfile['Flux']
+f3 = npzfile['f']
+Rem3 = npzfile['Rem1'] # magnetic Reynolds number from compositional (Nimmo) and thermal convection (whatever is larger at each time step)
+
+Fs3 = Flux2[0]
+Fcmb3 = Flux2[1]
+Fad3 = Flux2[2]
+Frad3 = Flux2[3]
+
+t_plot3 = t3/Myr
+
 # #import label info - read in from correct row in csv
-run_info = pd.read_csv('run_info3.csv',delimiter=',')
+run_info = pd.read_csv('run_info4.csv',delimiter=',')
 
 row = run_info[run_info['run']==run1]
 r = row.iloc[0,1] #radius [m]
@@ -67,13 +91,11 @@ dr=row.iloc[0,10] #cell spacing
 dt =row.iloc[0,11] #T_profile output frequency
 
 ################### Main Plot #########################################
-#choose model labels
-model1= 'Dodds et. al. (2021)'#'FK approx.'
-model2='Bryson et. al. (2019)'#'Arrhenius'
+
 # make  log-log plot - similar to Bryson 2019
 
 plt.figure(tight_layout=True)
-plt.suptitle('Thermal evolution of a {:.0f}km asteroid '.format(r/1e3, Tm0, Tsolidus))#\n Tm0 = {}K, Tsolidus ={}K 
+plt.suptitle('Thermal evolution of a {:.0f}km asteroid '.format(r/1e3, Tm0))#\n Tm0 = {}K, Tsolidus ={}K 
 
 xmin=tstart
 
@@ -111,38 +133,42 @@ plt.legend(loc='upper right',ncol=2,fontsize='small')
 #plt.savefig('Plots/Tflux_comp.png',dpi=450)
 
 ### Do same thing for Rem and f 
-plt.figure(tight_layout=True)
-plt.suptitle('Thermal evolution of a {:.0f}km asteroid \n Tm0 = {}K, Tsolidus ={}K'.format(r/1e3, Tm0, Tsolidus))
-plt.subplot(2,1,1)
-plt.loglog(t_plot1,Rem1,label=model1,color='royalblue')
-plt.loglog(t_plot2,Rem2,label=model2,color='orange',linestyle='--')
-#plt.xlim([xmin,max(t_plot)])
-plt.hlines(10,xmin=0,xmax=t_plot2[len(Rem1)-1],color='k',linestyle='--')
-#plt.xlabel('Time/Myr')
-plt.ylabel('Rem')
-plt.legend(loc='upper left')
-plt.ylim([1,100])
+with sns.plotting_context('talk',font_scale=0.8):
+    plt.figure(tight_layout=True,figsize=[10,7])
+    plt.suptitle('Thermal evolution of a {:.0f}km asteroid '.format(r/1e3))
+    plt.subplot(2,1,1)
+    plt.loglog(t_plot1,Rem1,label=model1,color='royalblue')
+    plt.loglog(t_plot2,Rem2,label=model2,color='cornflowerblue',linestyle='--')
+    plt.loglog(t_plot3,Rem3,label=model3,color='navy',linestyle='dotted')
+    #plt.xlim([xmin,max(t_plot)])
+    plt.hlines(10,xmin=0,xmax=t_plot2[-1],color='k',linestyle='--')
+    #plt.xlabel('Time/Myr')
+    plt.ylabel('Rem')
+    plt.legend(loc='upper left',ncol=2)
+    plt.ylim([1,100])
+    
+    plt.subplot(2,1,2)
+    plt.semilogx(t_plot1,f1,label=model1,color='royalblue')
+    plt.semilogx(t_plot2,f2,label=model2,color='cornflowerblue',linestyle='--')
+    plt.semilogx(t_plot3,f3,label=model3,color='navy',linestyle='dotted')
+    #plt.xlim([xmin,max(t_plot)])
+    plt.xlabel('Time/ Myr')
+    plt.ylabel('f')
+    plt.legend()
+    plt.savefig('Plots/Remf_comp_bbb.png',dpi=450)
 
-plt.subplot(2,1,2)
-plt.semilogx(t_plot1,f1,label=model1,color='royalblue')
-plt.semilogx(t_plot2,f2,label=model2,color='orange',linestyle='--')
-#plt.xlim([xmin,max(t_plot)])
-plt.xlabel('Time/ Myr')
-plt.ylabel('f')
-plt.legend()
-
-#plt.savefig('Plots/Remf_comp.png',dpi=450)
-
-################# Plot for transfer ##########################################
+################# Core and mantle temp plot for transfer ##########################################
 with sns.plotting_context('talk'):
     plt.figure(tight_layout=True,figsize=[10,7])
-    plt.title(f'Thermal evolution of a {r/1e3:.0f}km asteroid ')#\n Tm0 = {}K, Tsolidus ={}K 
+    plt.title(f'Thermal evolution of a {r/1e3:.0f}km asteroid ')
     xmin=tstart
     #temperatures as function of time
     plt.semilogx(t_plot1,Tm1,label='mantle',color='red')  
     plt.semilogx(t_plot1,Tc1,label='core',color='black')
     plt.semilogx(t_plot2,Tm2,color='red',linestyle='--')
     plt.semilogx(t_plot2,Tc2,color='black',linestyle='--',label=model2)
+    plt.semilogx(t_plot3,Tm3,color='red',linestyle='dotted',label=model3)
+    plt.semilogx(t_plot3,Tc3,color='black',linestyle='dotted',label=model1)
     plt.semilogx(t_plot1,Tc1,label=model1,color='black')
     plt.ylabel('T/K')
     plt.xlabel('Time/Myr')
