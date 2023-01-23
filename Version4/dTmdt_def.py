166afe38df0d44dd91b90bf1f0930c8affdddc37
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-01-23 10:27:51 +0000

    Added convective temperature change for undifferentiated body

--- Version4/dTmdt_def.py
+++ Version4/dTmdt_def.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Expression for dTm/dt from rearranging eqn 25 in Dodds (2020)
+Expression for dTm/dt from rearranging eqn 25 in Dodds (2020) and dTdt for an undifferentiated body
 """
-from parameters import rhom, cpm_p, Vm, As, Ts, h0, Al0, XAl, thalf_al, Acmb, km, gamma, Myr
+from parameters import rhom, cpm_p, Vm, As, Ts, h0, Al0, XAl, thalf_al, Acmb, km, gamma, Myr, cpa, rhoa, V
 import numpy as np
 from Rayleigh_def import Rayleigh_calc
 
@@ -32,3 +32,27 @@ def dTmdt_calc(t,Fs,Fcmb):
     
     
     return 1/(rhom*cpm_p*Vm)*(rad-Fs*As+Fcmb*Acmb)
+
+def dTadt_calc(t,Fs):
+    """
+
+    Parameters
+    ----------
+    t : float
+        time, s
+    Fs: float
+        surface heat flux [W m^-2]
+
+    Returns
+    -------
+    dTadt : float
+            rate of change of body temperature 
+
+    """
+    
+    #calculate radiogenic heating 
+    h = h0*Al0*XAl*np.exp(-np.log(2)*t/thalf_al) 
+    rad = h*rhoa*V #radiogenic heating contribution
+    
+    
+    return 1/(rhoa*cpa*V)*(rad-Fs*As)
