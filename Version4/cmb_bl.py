59da61627c88ccefcbe35df6b82fac9c26771688
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-02-28 15:45:45 +0000

    Changed d to r-rc in calculation of lower boundary layer too

--- Version4/cmb_bl.py
+++ Version4/cmb_bl.py
@@ -25,7 +25,7 @@ def delta_l(t,Tm,Tcmb):
     """
     eta = viscosity(Tm)
     Ra, d0, RaH, RanoH = Rayleigh_calc(t,Tm)
-    delta_l = (r-rc-d0)*(gamma*abs(Tcmb-Tm)/c1)**(4/3)*(Ra/Rac)**(-1/3)
+    delta_l = (r-rc)*(gamma*abs(Tcmb-Tm)/c1)**(4/3)*(Ra/Rac)**(-1/3)
 
     return delta_l
 
