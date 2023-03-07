0ddb20ddfc339b364dddc33d0022cfbb018508dd
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-03-07 12:56:00 +0000

    Corrected magma ocean thickness when convecting

--- Version4/dTmdt_def.py
+++ Version4/dTmdt_def.py
@@ -3,13 +3,13 @@
 """
 Expression for dTm/dt from rearranging eqn 25 in Dodds (2020) and dTdt for an undifferentiated body
 """
-from parameters import rhom, Vm, As, Ts, Acmb, km, gamma, Myr, cpa, rhoa, V
+from parameters import rhom, As, Ts, Acmb, km, gamma, Myr, cpa, rhoa, V, r, rc, Vm
 import numpy as np
 from Rayleigh_def import Rayleigh_calc
 from heating import Al_heating, AlFe_heating
 from cp_func import cp_calc_int
 
-def dTmdt_calc(t,Tconv,Fs,Fcmb):
+def dTmdt_calc(t,Tconv,d0,Fs,Fcmb):
     """
 
     Parameters
@@ -29,13 +29,16 @@ def dTmdt_calc(t,Tconv,Fs,Fcmb):
             rate of change of mantle temperature 
 
     """
-    
+    if (r-d0) < rc: #i.e. lid thickness is less than mantle thickness
+        Vocean = 4/3*np.pi*((r-d0)**3-rc**3)
+    else:
+        Vocean = Vm #put filler here as the output of this function won't be used
     #calculate radiogenic heating 
     h = Al_heating(t)
-    rad = h*rhom*Vm #radiogenic heating contribution
+    rad = h*rhom*Vocean #radiogenic heating contribution
     cp = cp_calc_int(Tconv,False)
     
-    return 1/(rhom*cp*Vm)*(rad-Fs*As+Fcmb*Acmb)
+    return 1/(rhom*cp*Vocean)*(rad-Fs*As+Fcmb*Acmb)
 
 def dTadt_calc(t,Tconv,Fs): #not sure if this is called anywhere
     """
--- Version4/full_euler.py
+++ Version4/full_euler.py
@@ -163,7 +163,7 @@ def thermal_evolution(tstart,tend,dt,T0,f0,sparse_mat_c,sparse_mat_m):
     else: #mantle is convecting replace mantle below stagnant lid with isothermal convective profile
         dl[0] = delta_l(tsolve[0],T0_mantle[1],T0_mantle[0])
         
-        Tm_conv[0] = T0_mantle[1] + dTmdt_calc(tsolve[0],T0_mantle[1],Fs[0],Fcmb[0])*dt #take temp one above CMB
+        Tm_conv[0] = T0_mantle[1] + dTmdt_calc(tsolve[0],T0_mantle[1],d0[0],Fs[0],Fcmb[0])*dt #take temp one above CMB
         T_new_mantle[:lid_start+1] = Tm_conv[0]
         T_new_mantle[-1] = Ts #pin surface to 200K
         Fs[0] = -km*(Ts-Tm_conv[0])/d0[0] #replace Fs with convective version
@@ -304,7 +304,7 @@ def thermal_evolution(tstart,tend,dt,T0,f0,sparse_mat_c,sparse_mat_m):
             else: #mantle just started convecting
                 Tm_old = T_old_mantle[1] # take temperature at base of mantle as mantle temp
 
-            Tm_conv[i] = Tm_old + dTmdt_calc(tsolve[i-1],Tm_old,Fs[i-1],Fcmb[i-1])*dt #temperature of convecting region 
+            Tm_conv[i] = Tm_old + dTmdt_calc(tsolve[i-1],Tm_old,d0[i-1],Fs[i-1],Fcmb[i-1])*dt #temperature of convecting region 
             T_new_mantle[:lid_start+1] = Tm_conv[i]
             T_new_mantle[-1] = Ts #pin surface to 200K in case d0 < 1 cell thick
             Fs[i] = -km*(Ts-Tm_conv[i])/d0[i]
