ce0aa21fdca11b1c32a3972b9638bea71f3683c1
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-03-10 17:07:04 +0000

    Investigated differentiation criteria

--- Version4/differentiation_criteria.py
+++ Version4/differentiation_criteria.py
@@ -4,31 +4,46 @@
 Investigate affect of stagnant lid criterion on differentiation time and temperature
 """
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from parameters import Myr
 
-data = np.loadtxt('lid_test.csv',delimiter=',',skiprows=1)
+data = pd.read_csv('lid_test.csv',delimiter=',',header=[0],skiprows=[1])
 
-t = data[:,0]/Myr #time in s
-T = data[:,1] #temp in K at onset of differentiation
-f = data[:,2] #fraction of stagnant lid thickness to total radius
+data.loc[data['core_conv']==np.inf,'core_conv'] = 50 #if core didn't start convecting 50 is a lower limit
 
-melt = np.array([1480,1520,1560,1600])
-colors=['black','darkblue','cornflowerblue','lightblue']
+labels = ['diff_time','Tdiff','peakT','tmax','strat_end','core_conv']
 
+######################## Absolute plots ######################################
+yaxis_lab = ['differentiation time/Myr','Temp at differentiation/K','Peak temperature /K','Time of peak temperature /Myr','End of core thermal stratification /Myr','Onset of core convection/Myr']
+for val, ylab in zip(labels[:-1],yaxis_lab[:-1]):
+    plt.figure(tight_layout=True)
+    plt.scatter(data['convect_ratio'],data[val])
+    plt.xlabel('$\delta_0$/r')
+    plt.ylabel(f'{ylab}')
+    plt.savefig(f'Plots/Lid_test/{val}_abs.png')
+    
+#plot core convection separately
 plt.figure()
-plt.scatter(f,T)
-for val, color in zip(melt,colors):
-    plt.hlines(val,0.001,0.9,label=f'{(val-1400)/4:.0f}% melt',linestyle='dashed',color=color,linewidth=2)
+plt.scatter(data.loc[data['core_conv']!=50,'convect_ratio'],data.loc[data['core_conv']!=50,'core_conv'])
+plt.scatter(data.loc[data['core_conv']==50,'convect_ratio'],data.loc[data['core_conv']==50,'core_conv'],label='lower limit')
 plt.xlabel('$\delta_0$/r')
-plt.ylabel('T at onset/K')
-plt.xscale('log')
+plt.ylabel('Onset of core convection/Myr')
 plt.legend()
+plt.savefig(f'Plots/Lid_test/core_conv_abs.png')
+##################### Percentage plots #######################################
+#Don't make one for convective core as not all cores started convecting
+for val, ylab in zip(labels[:-1],yaxis_lab[:-1]):
+    
+    plt.figure(tight_layout=True)
+    plt.scatter(data['convect_ratio'],(np.sqrt((data[val]-data[val].mean())**2)/data[val].mean()))
+    plt.xlabel('$\delta_0$/r')
+    plt.ylabel(f'Fractional RMS deviation in \n {ylab}')
+    plt.savefig(f'Plots/Lid_test/{val}_rel.png')
 
-
-plt.figure()
-plt.scatter(f,t)
+######## Differentiation temp as a fraction of peak temp ##################
+plt.figure(tight_layout=True)
+plt.scatter(data['convect_ratio'],data['Tdiff']/data['peakT'])
 plt.xlabel('$\delta_0$/r')
-plt.ylabel('t at onset/ Myr')
-plt.xscale('log')
-
+plt.ylabel(f'Differentiation temp/peak temp')
+plt.savefig(f'Plots/Lid_test/temp_compare.png')
