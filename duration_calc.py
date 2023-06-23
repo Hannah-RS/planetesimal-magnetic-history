02d0381469b409fa3e8ebb22da3a89dbbc2aad79
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-06-23 09:25:36 +0100

    Added edge case of no on periods

--- duration_calc.py
+++ duration_calc.py
@@ -32,17 +32,22 @@ def on_off_test(tarray,out_array,threshold,save_interval):
     """
     #check arrays are same length
     assert(len(tarray)==len(out_array))
-    t = tarray[out_array>threshold]
-    #find difference between sucessive tvalues
-    tdiff = np.ediff1d(t)
-    tend_ind = np.where(tdiff>save_interval)[0]
-    tend = t[tend_ind]
-    tstart_ind = np.where(tdiff>save_interval)[0]+1
-    tstart = t[tstart_ind]
-    #append first and last array values
-    tend = np.append(tend,t[-1])
-    tstart = np.insert(tstart, 0, t[0])
-    duration = tend-tstart
+    if np.all(out_array<threshold): #no on periods
+        tstart = np.array([np.nan])
+        tend = np.array([np.nan])
+        duration = np.array([np.nan])
+    else:
+        t = tarray[out_array>threshold]
+        #find difference between sucessive tvalues
+        tdiff = np.ediff1d(t)
+        tend_ind = np.where(tdiff>save_interval)[0]
+        tend = t[tend_ind]
+        tstart_ind = np.where(tdiff>save_interval)[0]+1
+        tstart = t[tstart_ind]
+        #append first and last array values
+        tend = np.append(tend,t[-1])
+        tstart = np.insert(tstart, 0, t[0])
+        duration = tend-tstart
     
     return tstart, tend, duration
 
@@ -73,12 +78,14 @@ def on_off_save(tarray,out_array,threshold,save_interval,file,label,run):
     start, end, duration = on_off_test(tarray,out_array,threshold,save_interval)
     data = {"run":run,"label":label,"start":start, "end":end, "duration":duration}
     data = pd.DataFrame(data)
-    data.to_csv(file,index=False)
+    data.to_csv(file,index=False,mode='a',header=False)
     return None
 
 #test function
 tarray = np.linspace(1,20,20)
-Rem = np.array([1,20,20,20,4,4,20,20,20,3,8,11,11,11,11,11,0,0,20,20])
+#Rem = np.array([1,20,20,20,4,4,20,20,20,3,8,11,11,11,11,11,0,0,20,20])
+#Rem = np.array([1,1,1,1,4,4,20,20,20,3,8,1,1,1,1,1,0,0,1,1])
+Rem = np.linspace(0,5,20)
 threshold =10
 save_interval = 1
 on_off_save(tarray,Rem,threshold,save_interval,'test_import.csv','MAC',1)
