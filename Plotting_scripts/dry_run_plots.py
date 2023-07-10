b2a972c946f130b5a52e6ee86272e9615beb4871
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-07-10 14:54:48 +0100

    Added peak core temp and and corrected for new end of mantle conduction

--- Plotting_scripts/dry_run_plots.py
+++ Plotting_scripts/dry_run_plots.py
@@ -10,13 +10,11 @@ import matplotlib.pyplot as plt
 import sys
 # setting path
 sys.path.append('../')
-from plotting_constants import Myr
 
 data = combine_info('../Results_combined/Xs_r_tests/','auto_params.csv','run_results.csv',['MAC_onoff.csv','CIA_onoff.csv','comp_onoff.csv','coreconv_onoff.csv'])
 data['r']=data['r']/1e3 #rescale to km
-data['cond_t'] = data['cond_t']/Myr #rescale by Myr
 
-save = True #do you want to save the figures
+save = False #do you want to save the figures
 ###################### Effect on dynamo timing ################################
 #r as x axis, sulfur as colour
 plt.figure(tight_layout=True,figsize=[15,15])
@@ -97,6 +95,11 @@ if save == True:
 make_scatter(data['r'], data['peakT'], 'radius /km', 'peak mantle temperature /K',colour=data['Xs_0'],colourlabel='X$_{S,0}$',ss=2.5)
 if save == True:
    plt.savefig('../Plots/Xs_r_tests/peakT_axis.png',dpi=450) 
+
+#r as x axis, time of peak as colour
+make_scatter(data['r'], data['peakT'], 'radius /km', 'peak mantle temperature /K',colour=data['tmax'],colourlabel='time of peak/Myr',ss=2.5)
+if save == True:
+   plt.savefig('../Plots/Xs_r_tests/peakT_time.png',dpi=450)
    
 # r as xaxis, sulfur as y, peak T as colour
 make_scatter(data['r'],data['Xs_0'],'radius /km','X$_{S,0}$',colour=data['peakT'],colourlabel='peak mantle temperature /K',ss=2.5)
@@ -107,8 +110,17 @@ if save == True:
 # r as xaxis, sulfur as y, peak T as colour
 make_scatter(data['r'],data['Xs_0'],'radius /km','X$_{S,0}$',colour=data['peak_coreT'],colourlabel='peak core temperature /K',ss=2.5)
 if save == True:
-   plt.savefig('../Plots/Xs_r_tests/peakT_colour.png',dpi=450)
-   
+   plt.savefig('../Plots/Xs_r_tests/peakcoreT_colour.png',dpi=450)
+
+#r as x axis, sulfur as colour
+make_scatter(data['r'], data['peak_coreT'], 'radius /km', 'peak core temperature /K',colour=data['Xs_0'],colourlabel='X$_{S,0}$',ss=2.5)
+if save == True:
+   plt.savefig('../Plots/Xs_r_tests/peakcoreT_axis.png',dpi=450) 
+
+#r as x axis, time of peak as colour
+make_scatter(data['r'], data['peak_coreT'], 'radius /km', 'peak core temperature /K',colour=data['tcoremax'],colourlabel='time of peak/Myr',ss=2.5)
+if save == True:
+   plt.savefig('../Plots/Xs_r_tests/peakcoreT_time.png',dpi=450)   
 #################### Thermal history timings ###############################
 #y axis is r, x axis is time
 plt.figure(figsize=[15,5])
@@ -117,7 +129,7 @@ plt.scatter(data['diff_time'],data['r'],label='differentiation',marker='x',c=dat
 plt.scatter(data['tmax'],data['r'],label='peak mantle temp',marker='+',c=data['Xs_0'])
 plt.scatter(data['tstrat_remove'],data['r'],label='erosion of core stratification',marker='o',c=data['Xs_0'])
 plt.scatter(data['terode'],data['r'],label='core stratification removed',marker='v',c=data['Xs_0'])
-plt.scatter(data['cond_t'],data['r'],label='end of mantle convection',marker='*',c=data['Xs_0'])
+plt.scatter(data['fcond_t'],data['r'],label='end of mantle convection',marker='*',c=data['Xs_0'])
 plt.scatter(data['tsolid'],data['r'],label='core solidified',marker='^',c=data['Xs_0'])
 plt.xlabel('Time/Myr')
 plt.xscale('log')
@@ -162,7 +174,7 @@ plt.scatter(data['start_MAC'],data['r'],label='MAC dynamo start',marker='<',c=da
 plt.scatter(data['end_MAC'],data['r'],label='MAC dynamo end',marker='>',c=data['Xs_0'])
 plt.scatter(data['start_CIA'],data['r'],label='CIA dynamo start',marker='3',c=data['Xs_0'])
 plt.scatter(data['end_CIA'],data['r'],label='CIA dynamo end',marker='4',c=data['Xs_0'])
-plt.scatter(data['cond_t'],data['r'],label='end of mantle convection',marker='*',c=data['Xs_0'])
+plt.scatter(data['fcond_t'],data['r'],label='end of mantle convection',marker='*',c=data['Xs_0'])
 plt.scatter(data['start_comp'],data['r'],label='compositional dynamo start',marker='^',c=data['Xs_0'])
 plt.scatter(data['end_comp'],data['r'],label='compositional dynamo end',marker='v',c=data['Xs_0'])
 plt.scatter(data['tsolid'],data['r'],label='core solidified',marker='d',c=data['Xs_0'])
@@ -195,7 +207,7 @@ plt.scatter(data_fil['start_MAC'],data_fil['r'],label='MAC dynamo start',marker=
 plt.scatter(data_fil['end_MAC'],data_fil['r'],label='MAC dynamo end',marker='>',color='darkblue')
 plt.scatter(data_fil['start_CIA'],data_fil['r'],label='CIA dynamo start',marker='3',color='royalblue')
 plt.scatter(data_fil['end_CIA'],data_fil['r'],label='CIA dynamo end',marker='4',color='royalblue')
-plt.scatter(data_fil['cond_t'],data_fil['r'],label='end of mantle convection',marker='*',color='palevioletred')
+plt.scatter(data_fil['fcond_t'],data_fil['r'],label='end of mantle convection',marker='*',color='palevioletred')
 plt.scatter(data_fil['start_comp'],data_fil['r'],label='compositional dynamo start',marker='^',color='skyblue')
 plt.scatter(data_fil['end_comp'],data_fil['r'],label='compositional dynamo end',marker='v',color='skyblue')
 plt.scatter(data_fil['tsolid'],data_fil['r'],label='core solidified',marker='d',color='pink')
@@ -216,7 +228,7 @@ plt.scatter(data['start_MAC'],data['r'],label='MAC dynamo start',marker='<',c=da
 plt.scatter(data['end_MAC'],data['r'],label='MAC dynamo end',marker='>',c=data['Xs_0'])
 plt.scatter(data['start_CIA'],data['r'],label='CIA dynamo start',marker='3',c=data['Xs_0'])
 plt.scatter(data['end_CIA'],data['r'],label='CIA dynamo end',marker='4',c=data['Xs_0'])
-plt.scatter(data['cond_t'],data['r'],label='end of mantle convection',marker='*',c=data['Xs_0'])
+plt.scatter(data['fcond_t'],data['r'],label='end of mantle convection',marker='*',c=data['Xs_0'])
 plt.xlabel('Time/Myr')
 plt.xscale('log')
 plt.xlim(right=10)
