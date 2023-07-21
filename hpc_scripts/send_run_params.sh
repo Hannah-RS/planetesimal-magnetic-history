ec1841a57177c2b14ef408e8e738c199508ca133
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-07-21 15:45:04 +0100

    Added changing to parent directory first

--- hpc_scripts/send_run_params.sh
+++ hpc_scripts/send_run_params.sh
@@ -1,2 +1,3 @@
 #send a folder of run parameters to the hpc
+cd
 scp -r Documents/Code/learning-model/Run_params/$1/ exet5460@gateway.arc.ox.ac.uk://data/eart-astroid-evo/exet5460/Asteroid_model/Run_params
