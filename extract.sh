70cbd8e86c4706348a7d17fd159816f1567327c9
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-07-24 12:26:10 +0100

    Corrected copying file names

--- extract.sh
+++ extract.sh
@@ -4,10 +4,10 @@
 #copy data across 
 
 mkdir Results_combined/$1 #make directory for results
-scp -r exet5460@gateway.arc.ox.ac.uk://data/eart-astroid-evo/exet5460/Asteroid_model/Results/$1 Results_combined/$1/ #copy from hpc
+scp -r exet5460@gateway.arc.ox.ac.uk://data/eart-astroid-evo/exet5460/Asteroid_model/Results/$1/ Results_combined/$1/ #copy from hpc
 nfolders=$(ls Results_combined/$1|wc -l) #count number of folders
-cp all_info.csv Results_combined/$1/all_sucess_info.csv #copy a template for final sucess results
-cp all_info.csv Results_combined/$1/all_fail_info.csv #copy a template for final fail results
+cp all_sucess_info.csv Results_combined/$1/all_sucess_info.csv #copy a template for final sucess results
+cp fail_params.csv Results_combined/$1/fail_params.csv #copy a template for final fail results
 
 python merge_results.py $1/ $nfolders #merge results, give folder and number of subfolders as arguments
 
