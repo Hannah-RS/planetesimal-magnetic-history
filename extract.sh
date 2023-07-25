df5a7a02bf174ec7805f024b24f95fc99c8fd6ce
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-07-25 09:37:19 +0100

    Automated making hpc directory and moving results there

--- extract.sh
+++ extract.sh
@@ -1,10 +1,17 @@
 #script to copy results off the hpc and merge them together
 #give the folder name on the command line
-#run the command from learning-model folder
-#copy data across 
+#run the command from learning-model folder 
 
+#login to hpc
+ssh exet5460@arc-login.arc.ox.ac.uk
+cd $DATA/Asteroid_model/Results
+mkdir $1 #make directory
+mv params_* $1/ #move results to directory
+logout #logout of hpc
+
+#on local machine
 mkdir Results_combined/$1 #make directory for results
-scp -r exet5460@gateway.arc.ox.ac.uk://data/eart-astroid-evo/exet5460/Asteroid_model/Results/$1/ Results_combined/$1/ #copy from hpc
+scp -r exet5460@gateway.arc.ox.ac.uk://data/eart-astroid-evo/exet5460/Asteroid_model/Results/$1/* Results_combined/$1/ #copy from hpc
 nfolders=$(ls Results_combined/$1|wc -l) #count number of folders
 cp all_sucess_info.csv Results_combined/$1/all_sucess_info.csv #copy a template for final sucess results
 cp fail_params.csv Results_combined/$1/fail_params.csv #copy a template for final fail results
