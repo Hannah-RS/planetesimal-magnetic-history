4615b16e0b61ba3bca9b2771290b86ca160d1428
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-08-21 16:48:44 +0100

    Reordered to follow order of csv numbering

--- create_single_csv.py
+++ create_single_csv.py
@@ -6,14 +6,9 @@ Script to create auto_params.csv for chosen parameters ranges
 import pandas as pd
 import numpy as np
  
-folder = 'Run_params/Minirun3/' #folder to save parameters in   
+folder = 'Run_params/Singlevar2/' #folder to save parameters in   
 #for parameter list
 #each variable
-# #radius [m]
-nr = 4 #number of points
-minr = 100e3 #min val
-maxr = 400e3 #max val
-r = np.linspace(minr,maxr,nr) 
 
 #rcmf
 # nrcmf = 6
@@ -22,9 +17,9 @@ r = np.linspace(minr,maxr,nr)
 # rcmf = np.linspace(minrcmf,maxrcmf,nrcmf)
 
 # #reference viscosity
-# neta0 = 16
-# mineta0 = 10 #10^{mineta0}
-# maxeta0 = 25 
+# neta0 = 10
+# mineta0 = 14 #10^{mineta0}
+# maxeta0 = 23 
 # eta0 = np.logspace(mineta0,maxeta0,neta0)
 
 # #beta
@@ -33,46 +28,56 @@ r = np.linspace(minr,maxr,nr)
 # maxbeta = 0.08
 # beta = np.linspace(minbeta,maxbeta,nbeta)
 
+# #liquid viscosity
+# netal = 3
+# minetal = 0 #log10(etal)
+# maxetal = 2 #log10(etal)
+# etal = np.logspace(minetal,maxetal,3)
+
 # #sulfur content
 # nxs = 3
-# minxs = 28.5 #will vary by body size later
+# minxs = 27 #minimum for a 300km radius, rcmf = 0.3 
 # maxxs = 32
 # Xs_0 = np.linspace(minxs,maxxs,nxs)
 
 # #iron content - specified manually for now
-# nfe = 3
+# nfe = 4
 # minfe = 0
 # maxfe = 1e-7
-# Fe0 = np.array([0,1e-8,1e-7])
-
-# #liquid viscosity
-# netal = 3
-# minetal = 1 #log10(etal)
-# maxetal = 3 #log10(etal)
-# etal = np.logspace(minetal,maxetal,3)
+# Fe0 = np.array([0,1e-8,1e-7,6e-7])
 
 #diffusion vs dislocation creep
 #alpha_n = np.array([25,30]) #diffusion vs (diffusion+dislocation) creep (S&C)
 
+# #radius [m]
+nr = 6 #number of points
+minr = 50e3 #min val
+maxr = 500e3 #max val
+r = np.array([50e3,100e3,200e3,300e3,400e3,500e3])
 
 #parameters fixed in this combination
 default = 'vary' #viscosity profile will change
 t_acc_m = 0.8 #accretion time [Myr]
-t_end_m = 1000 #maximum end time of simulation [Myr]
+t_end_m = 1200 #maximum end time of simulation [Myr]
 w = 5 #width of linear region [K]
-etalval = 100 #liquid viscosity [Pas]
 dr = 500 # grid space [m]
 dt = 0.075 #timestep [fraction of core conductive timestep]
-alpha_nval = 25 #melt weakening, diffusion creep
-#rval=300e3
-feval = 1e-7
-xsval = 30
+
+rcmfval= 0.3
 eta0val=1e21
 betaval = 0.011
-rcmfval= 0.3
+etalval = 100 #liquid viscosity [Pas]
+xsval = 29.5
+feval = 1e-7
+alpha_nval = 25 #melt weakening, diffusion creep
+#rval=300e3
+
+
+
+
 
 # create dictionaries and write to csv - change these three lines
-run = 39
+run = 34
 csv_num = 8
 var = r #choose your variable
 
