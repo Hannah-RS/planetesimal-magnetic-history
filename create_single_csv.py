7165e2ca1a37256f120f39e6c2f43ef198e6841a
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2023-10-20 15:18:36 +0100

    Updated parameter ranges

--- create_single_csv.py
+++ create_single_csv.py
@@ -6,78 +6,88 @@ Script to create auto_params.csv for chosen parameters ranges
 import pandas as pd
 import numpy as np
  
-folder = 'Run_params/Singlevar2/' #folder to save parameters in   
+folder = 'Run_params/Paper_run1/' #folder to save parameters in   
 #for parameter list
 #each variable
 
+#%%
 #rcmf
-# nrcmf = 6
-# minrcmf = 0.2
-# maxrcmf = 0.5
-# rcmf = np.linspace(minrcmf,maxrcmf,nrcmf)
+nrcmf = 6
+minrcmf = 0.2
+maxrcmf = 0.5
+rcmf = np.linspace(minrcmf,maxrcmf,nrcmf)
 
-# #reference viscosity
-# neta0 = 10
-# mineta0 = 14 #10^{mineta0}
-# maxeta0 = 23 
-# eta0 = np.logspace(mineta0,maxeta0,neta0)
+#%%
+#reference viscosity
+neta0 = 11
+mineta0 = 14 #10^{mineta0}
+maxeta0 = 24 
+eta0 = np.logspace(mineta0,maxeta0,neta0)
 
-# #beta
-# nbeta = 5
-# minbeta = 0.005
-# maxbeta = 0.08
-# beta = np.linspace(minbeta,maxbeta,nbeta)
+#%%
+#beta
+nbeta = 5
+minbeta = 0.01 #from Dodds 2021
+maxbeta = 0.035 #for E = 570 kj/mol 
+beta = np.linspace(minbeta,maxbeta,nbeta)
 
-# #liquid viscosity
-# netal = 3
-# minetal = 0 #log10(etal)
-# maxetal = 2 #log10(etal)
-# etal = np.logspace(minetal,maxetal,3)
+#%%
+#liquid viscosity
+netal = 3
+minetal = 0 #log10(etal)
+maxetal = 2 #log10(etal)
+etal = np.logspace(minetal,maxetal,3)
 
-# #sulfur content
-# nxs = 3
-# minxs = 27 #minimum for a 300km radius, rcmf = 0.3 
-# maxxs = 32
-# Xs_0 = np.linspace(minxs,maxxs,nxs)
+#%%
+#sulfur content
+nxs = 3
+minxs = 27 #minimum for a 300km radius, rcmf = 0.3 
+maxxs = 32
+Xs_0 = np.linspace(minxs,maxxs,nxs)
 
-# #iron content - specified manually for now
-# nfe = 4
-# minfe = 0
-# maxfe = 1e-7
-# Fe0 = np.array([0,1e-8,1e-7,6e-7])
+#%%
+#iron content - specified manually for now
+nfe = 5
+minfe = 0
+maxfe = 6e-7
+Fe0 = np.array([0,1e-9,1e-8,1e-7,6e-7])
 
+#%%
 #diffusion vs dislocation creep
-#alpha_n = np.array([25,30]) #diffusion vs (diffusion+dislocation) creep (S&C)
+nalpha = 5
+minalpha = 25
+maxalpha = 45
+alpha_n = np.linspace(minalpha,maxalpha,nalpha) #diffusion vs (diffusion+dislocation) creep (S&C)
 
+#%%
 # #radius [m]
-nr = 6 #number of points
-minr = 50e3 #min val
+nr = 5 #number of points
+minr = 100e3 #min val
 maxr = 500e3 #max val
-r = np.array([50e3,100e3,200e3,300e3,400e3,500e3])
+r = np.array([100e3,200e3,300e3,400e3,500e3])
 
-#parameters fixed in this combination
+#%%
+# parameters that are always fixed
 default = 'vary' #viscosity profile will change
 t_acc_m = 0.8 #accretion time [Myr]
-t_end_m = 1200 #maximum end time of simulation [Myr]
+t_end_m = 1500 #maximum end time of simulation [Myr]
 w = 5 #width of linear region [K]
 dr = 500 # grid space [m]
 dt = 0.075 #timestep [fraction of core conductive timestep]
 
+#parameters fixed in this combination - take middle value of each range
 rcmfval= 0.3
-eta0val=1e21
-betaval = 0.011
+eta0val= 1e19
+betaval = 0.0225
 etalval = 100 #liquid viscosity [Pas]
 xsval = 29.5
-feval = 1e-7
-alpha_nval = 25 #melt weakening, diffusion creep
+feval = 1e-8
+alpha_nval = 30 #melt weakening, diffusion creep
 #rval=300e3
 
-
-
-
-
+#%%
 # create dictionaries and write to csv - change these three lines
-run = 34
+run = 41
 csv_num = 8
 var = r #choose your variable
 
