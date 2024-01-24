1c3836ea9b68ad0bc5c4f4b2f2d08210d70a0924
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2024-01-24 15:00:04 +0000

    Only plot mantle temperature and ignore time information

--- Plotting_scripts/peakT.py
+++ Plotting_scripts/peakT.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib as mpl
 #%% Set up folders etc.
 folder = 'Paper_run4/'
+savefolder = 'EPSL_paper'
 subfolders = {'rcmf':1,'eta0':2,'beta':3,'etal':4,'Xs_0':5,'Fe0':6,'alpha_n':7,'r':8}
 labels = {'rcmf':'$\\phi_{{C}}$','eta0':'$\\eta_0$','beta':'$\\beta$','etal':'$\\eta_l$ ','Xs_0':'$X_{{s,0}}$','Fe0':'$^{{60}}Fe/^{{56}}Fe$','alpha_n':'$\\alpha_n$','r':'radius'}
 units = {'rcmf':'','eta0':'Pas','beta':'$K^{-1}$','etal':'Pas','Xs_0':'wt %','Fe0':'','alpha_n':'','r':'km'}
@@ -52,25 +53,26 @@ for i, var in enumerate(variables):
     #make the figure
     phi = (data['peakT']-1400)/400
     fig, ax1 = plt.subplots(nrows=1,ncols=1,sharex=True)
-    ax1.scatter(data[var],data['peakT'],label='Peak mantle temperature',marker='o',c=data['tmax'],vmin=tmin,vmax=tmax)
-    ax1.scatter(data[var],data['peak_coreT'],label='Peak core temperature',marker='v',c=data['tcoremax'],vmin=tmin,vmax=tmax)
+    ax1.scatter(data[var],data['peakT'],label='Peak mantle temperature',marker='o')
+    #ax1.scatter(data[var],data['peakT'],label='Peak mantle temperature',marker='o',c=data['tmax'],vmin=tmin,vmax=tmax)
+    #ax1.scatter(data[var],data['peak_coreT'],label='Peak core temperature',marker='v',c=data['tcoremax'],vmin=tmin,vmax=tmax)
     ax2 = ax1.twinx()
     #ax2.scatter(data[var],phi,marker='o',c=data['tmax'],vmin=tmin,vmax=tmax)
-    ax1.set_xlabel(varlab)
-    ax1.legend(loc='upper left')
-    ax1.set_ylabel('Temperature /K')
+    ax1.set_xlabel(f'Critical melt fraction, {varlab}')
+    #ax1.legend(loc='upper left')
+    ax1.set_ylabel('Peak Mantle Temperature /K')
     ax2.set_ylabel('Melt fraction')
     ax2.set_ylim([0.15,0.6])
     ax1.set_ylim([0.15*400+1400,0.6*400+1400])
     #add colorbar
 
-    norm = mpl.colors.Normalize(vmin=tmin,vmax=tmax)
-    cax = fig.add_axes([0.75, 0.2, 0.01, 0.3])
-    mpl.colorbar.ColorbarBase(cax, cmap='viridis', norm=norm, orientation='vertical',label='Time of maxima/Myr')
+    #norm = mpl.colors.Normalize(vmin=tmin,vmax=tmax)
+    #cax = fig.add_axes([0.75, 0.2, 0.01, 0.3])
+    #mpl.colorbar.ColorbarBase(cax, cmap='viridis', norm=norm, orientation='vertical',label='Time of maxima/Myr')
 
     #plt.xscale('log')
     if save == True:
-        plt.savefig(f'../Plots/{folder}/peakT_{var}.png')
+        plt.savefig(f'../Plots/{savefolder}/peakT_{var}.pdf')
         
 
         
