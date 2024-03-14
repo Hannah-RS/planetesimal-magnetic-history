6b614df4ae9ae44239920a316907d3fff9ee5da5
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2024-03-14 09:12:35 +0000

    Filter by 60Fe and distinguish compositional vs thermal dynamos

--- Analysis/peakB_allr.py
+++ Analysis/peakB_allr.py
@@ -9,24 +9,32 @@ import pandas as pd
 
 
 #%% Load data for chosen variable
-folders = ['Paper_run100km/','Paper_run200km/','Paper_run4/','Paper_run400km/','Paper_run500km/']
-peakB =[]
+folders = ['Paper_run100km/','Paper_run200km/','Paper_run300km/','Paper_run400km/','Paper_run500km/']
+peakBtherm =[]
+peakBcomp =[]
 max_dur = []
 min_dur = []
-for folder in folders:
+
+fefilter = False #do you only want 1e-8 60Fe/56Fe
+
+for i, folder in enumerate(folders):
     
     var_results = pd.read_csv(f'../Results_combined/{folder}/all_sucess_info.csv',skiprows=[1])
-    
-    if folder == 'Paper_run4/': #exclude radius runs
-        peakB.append(max(var_results.loc[var_results['r']==300000,'max_B'])/1e-6)
+    if fefilter == True:
+        var_results = var_results[var_results['Fe0']==1e-8]
+    if folder == 'Paper_run300km/': #exclude radius runs
+        peakBtherm.append(max(var_results.loc[var_results['r']==300000,'max_Btherm'])/1e-6)
+        peakBcomp.append(max(var_results.loc[var_results['r']==300000,'max_Bcomp'])/1e-6)
         dur = var_results.loc[var_results['r']==300000,'magoff_1'] - var_results.loc[var_results['r']==300000,'magon_1']
         max_dur.append(max(dur))
         min_dur.append(min(dur[dur>0]))
     else:
-        peakB.append(max(var_results['max_B'])/1e-6)
+        peakBtherm.append(max(var_results['max_Btherm'])/1e-6)
+        peakBcomp.append(max(var_results['max_Bcomp'])/1e-6)
         dur = var_results['magoff_1'] - var_results['magon_1']
         max_dur.append(max(dur))
         min_dur.append(min(dur[dur>0]))
-print('Peak field strength is',np.round(peakB,0))
+print('Peak thermal field strength is',np.round(peakBtherm,0))
+print('Peak compositional field strength is',np.round(peakBcomp,0))
 print('Minimum non-zero field duration is',np.round(min_dur,0))
 print('Maximum field duration is',np.round(max_dur,0))
