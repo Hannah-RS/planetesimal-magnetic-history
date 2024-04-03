2936054a3db563b9674f43beace31b32dd3e0cb0
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2024-04-03 09:55:05 +0100

    Add variable mantle density

--- Analysis/psyche_core_radius.py
+++ Analysis/psyche_core_radius.py
@@ -8,19 +8,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append('../')
+
 from fe_fes_liquidus import fe_fes_density
 
 #%% Define functions
-def weight_perc_fe(rhoc,Xs0):
+def weight_perc_fe(Xs0,rhoc,rhom,rhoa):
     """
 
     Parameters
     ----------
-    rhoc : float
-        core density [kg m^-3]
     Xs0 : float
         initial core sulfur content [wt %]
-
+    rhoc : float
+        core density [kg m^-3]
+    rhom : float
+        mantle density [kg m^-3]
+    rhoa : float
+        bulk density [kg m^-3]
+        
     Returns
     -------
     XFe : float
@@ -30,13 +35,17 @@ def weight_perc_fe(rhoc,Xs0):
     XFe = (rhoc/rhoa)*((rhoa-rhom)/(rhoc-rhom))*(1-0.01*Xs0)
     return XFe
 
-def core_radius(Xs0):
+def core_radius(Xs0,rhom,rhoa):
     """
 
     Parameters
     ----------
     Xs0 : float
         initial core sulfur content [wt %]
+    rhom : float
+        mantle density [kg m^-3]
+    rhoa : float
+        bulk density [kg m^-3]
 
     Returns
     -------
@@ -45,31 +54,32 @@ def core_radius(Xs0):
 
     """
     rhoc = fe_fes_density(Xs0)
-    XFe = weight_perc_fe(rhoc,Xs0)
+    XFe = weight_perc_fe(Xs0,rhoc,rhom,rhoa)
     rc = ((XFe/(1-0.01*Xs0))*(rhoa/rhoc))**(1/3)
     return rc
 
 #%% Set parameters
-rhom = 3000 # mantle density [kg m^-3] Bryson et. al. (2019)
+rhoms = np.array([2500,3000,3500]) # mantle density [kg m^-3] Johnson et. al. (2020)
 rhoa = 4000 # bulk density [kg m^-3]
 
 #%% Create arrays for output
 Xs0 = np.linspace(0,33,33)
 
-rc = core_radius(Xs0)
-
 #%% Plot result for rc
+colors = ['navy','mediumblue','skyblue']
+lines = ['-','-.',':']
 fig, ax = plt.subplots(1,1)
-ax.plot(Xs0,rc)
-ax.set(ylabel='$\\frac{r_c}{r}$',xlabel='Initial core sulfur content/ wt %')
-ax.hlines(core_radius(27.1),0,27.1,linestyle='dashed',color='black')
-ax.vlines(27.1,core_radius(0),core_radius(27.1),linestyle='dashed',color='black')
-ax.text(0,0.89,f'{core_radius(27.1):.2f}')
-ax.text(27.2,0.63,'27.1')
-
+ax.grid(which='both',alpha=0.5)
+for rhom, color, ls in zip(rhoms,colors,lines):
+    rc = core_radius(Xs0,rhom,rhoa)
+    ax.plot(Xs0,rc,label=f'$\\rho_m =${rhom} $kg m^{{-3}}$',color=color,linestyle=ls)
+ax.set(ylabel='Core radius fraction - $\\frac{r_c}{r}$',xlabel='Initial core sulfur content/ wt %')
+ax.legend()
+ax.set_title('bulk density 4000 kg$m^{-3}$')
+fig.savefig('../Plots/Psyche/core_radius_frac.png',dpi=500)
 #%% Plot results for XFe
 rhoc = fe_fes_density(Xs0)
-Xfe = weight_perc_fe(rhoc,Xs0)
+Xfe = weight_perc_fe(Xs0,rhoc,rhoms[1],rhoa)
 maxfe = weight_perc_fe(fe_fes_density(27.1),27.1)
 fig, ax = plt.subplots(1,1)
 ax.plot(Xs0,Xfe)
