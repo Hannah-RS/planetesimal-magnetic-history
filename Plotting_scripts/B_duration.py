69db2a8f3041d788399d7884399665640689c23e
HannahRS <hrs46@cantab.ac.uk>
2024-04-23 15:39:06 +0100

    Add tickabels

--- Plotting_scripts/B_duration.py
+++ Plotting_scripts/B_duration.py
@@ -24,7 +24,7 @@ ytick_val2 = []
 runval = np.array([])
 
 
-save = False
+save = True
 xmax = 12 #how much to zoom by
 #%% Load a given variable
 
@@ -48,24 +48,13 @@ for i, var in enumerate(variables[:-1]):
     nrun = len(data)
     paths.extend([path]*nrun) #save paths for opening files later
     runval = np.concatenate([runval,var_data['run'].to_numpy()])
-    if logvar == True:
-        ytick_lab.append(f'{varlab}={var1:.1e}{unit}')
-        ytick_lab.extend(['']*(2*nrun-2))
-        ytick_lab.append(f'{varlab}={var2:.1e}{unit}')
-    else:
-        ytick_lab.append(f'{varlab}={var1:.1g}{unit}')
-        ytick_lab.extend(['']*(2*nrun-2))
-        ytick_lab.append(f'{varlab}={var2:.1g}{unit}')   
-    
-    #for labelling midpoints
-    ytick_lab2.append(varlab)   
-    ytick_val2.append(len(ytick_lab)-nrun) #len(ytick_lab)-nrun/2 but yplot gets doubled later, so x2
-        
+    ytick_lab = np.concatenate([ytick_lab,data[f'{var}'],['']]) #add a gap 
+
 #%% Massive stacked barchart
 nruns = len(runval)
-yplot = np.arange(2*nruns+1)
+yplot = np.arange(2*(nruns+7)) #add extra blank space
 #convert yticks to array
-ytick_arr = np.array([ytick_lab])
+
 
 #make colormap for B data
 import copy
@@ -84,7 +73,8 @@ bounds2 = [0.5,1]
 norm2 = mpl.colors.BoundaryNorm(bounds2, cmap2.N)
 
 fig, axes = plt.subplots(1,2,sharey='row',figsize=[7.5,5],gridspec_kw={'width_ratios': [1,3]},tight_layout=True)
-for ax in axes: 
+for ax in axes:
+    j = 0 #for plotting
     for i in range(nruns):
         #load Rem data
         run = int(runval[i])
@@ -109,20 +99,25 @@ for ax in axes:
         B[(t<tsolid_start)&(Rem<Rem_c)]=0
         #Bminfind.append(np.min(B[B>0]))
         
-        #make the plot
-        ax.pcolormesh(t,yplot[2*i:2*i+2],[B,B],cmap=cmap,norm=norm)
+        #set up ticks
+        if (ytick_lab[j]==''): #don't plot in gaps
+            ax.hlines(yplot[2*j],0.8,210,color='grey',linestyle='dashed',alpha=0.5,linewidth=0.5)
+            j=j+1
         
-        if (ytick_lab[2*i]!='')&(ytick_lab[2*i-1]!='')&(i>0):
-            ax.hlines(yplot[2*i]-1,0.8,210,color='black',linestyle='dashed',linewidth=0.5)
-        ax.pcolormesh(t,yplot[2*i+1:2*i+3],np.zeros([2,len(Rem)]),cmap=cmap2,norm=norm2) #add gaps between variables
-        ax.vlines(var_results.loc[var_results['run']==run,'tsolid_start'],yplot[2*i]-0.5,yplot[2*i]+0.5,color='black')
-         
+        #make the plot
+        ax.pcolormesh(t,yplot[2*j:2*j+2],[B,B],cmap=cmap,norm=norm)
+        
+        #create blank spaces between runs        
+        ax.pcolormesh(t,yplot[2*j+1:2*j+3],np.zeros([2,len(Rem)]),cmap=cmap2,norm=norm2) #add gaps between variables
+        ax.vlines(var_results.loc[var_results['run']==run,'tsolid_start'],yplot[2*j]-0.5,yplot[2*j]+0.5,color='black')
+        j = j+1
     
     ax.set_ylim([yplot[0]-1,yplot[-1]+0.5])
 axes[1].set_xlabel('Time after CAI formation /Ma') 
 axes[0].set_xlim([0.8,xmax])
 axes[1].set_xlim([xmax,300])
-axes[0].set_yticks(ytick_val2,ytick_lab2)
+axes[0].set_yticks(yplot[::2],ytick_lab)
+axes[0].tick_params(axis='y', labelsize=7) #make y labels smaller
 axes[1].set_title(f'>{xmax} Ma')
 axes[0].set_title(f'First {xmax} Ma')
 cax = fig.add_axes([0.87, 0.17, 0.01, 0.3])
@@ -132,7 +127,7 @@ colorbar.set_ticks([3,10,20,30,40],labels=[3,10,20,30,40])
 
 
 if save == True:
-    plt.savefig(f'../Plots/{savefolder}Rem_bars_full.png',dpi=450,bbox_inches='tight') 
+    plt.savefig(f'../Plots/{savefolder}B_duration.pdf',dpi=500,bbox_inches='tight') 
 
 #%% Minimum value for colormap
 #print(np.min(Bminfind)) #muT
