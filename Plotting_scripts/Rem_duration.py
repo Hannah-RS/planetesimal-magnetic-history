e60a359b4e03a7697339ee53961b4d1c6eda1903
HannahRS <hrs46@cantab.ac.uk>
2024-04-23 17:26:23 +0100

    Update x axis labels

--- Plotting_scripts/Rem_duration.py
+++ Plotting_scripts/Rem_duration.py
@@ -11,7 +11,7 @@ from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 
 
 #%% Load data for chosen variable
-folder = 'Paper_run4/'
+folder = 'Paper_run300km/'
 savefolder = 'EPSL_paper/'
 from plot_params import subfolders, labels, units, logs, variables, Myr
  
@@ -24,7 +24,7 @@ ytick_val2 = []
 runval = np.array([])
 
 
-save = True
+save = False
 xmax = 12 #how much to zoom by
 #%% Load a given variable
 
@@ -48,24 +48,11 @@ for i, var in enumerate(variables):
     nrun = len(data)
     paths.extend([path]*nrun) #save paths for opening files later
     runval = np.concatenate([runval,var_data['run'].to_numpy()])
-    if logvar == True:
-        ytick_lab.append(f'{varlab}={var1:.1e}{unit}')
-        ytick_lab.extend(['']*(2*nrun-2))
-        ytick_lab.append(f'{varlab}={var2:.1e}{unit}')
-    else:
-        ytick_lab.append(f'{varlab}={var1:.1g}{unit}')
-        ytick_lab.extend(['']*(2*nrun-2))
-        ytick_lab.append(f'{varlab}={var2:.1g}{unit}')   
+    ytick_lab = np.concatenate([ytick_lab,data[f'{var}'],['']]) #add a gap 
     
-    #for labelling midpoints
-    ytick_lab2.append(varlab)   
-    ytick_val2.append(len(ytick_lab)-nrun) #len(ytick_lab)-nrun/2 but yplot gets doubled later, so x2
-        
 #%% Massive stacked barchart
 nruns = len(runval)
-yplot = np.arange(2*nruns+1)
-#convert yticks to array
-ytick_arr = np.array([ytick_lab])
+yplot = np.arange(2*(nruns+7))
 
 #make colormap for Rem data
 cmap = (mpl.colors.ListedColormap([ '#7D9CEB', '#DDAA33']).with_extremes(over='#BB5566', under='#FFFFFF'))
@@ -73,13 +60,9 @@ cmap = (mpl.colors.ListedColormap([ '#7D9CEB', '#DDAA33']).with_extremes(over='#
 bounds = [10,40,100]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 
-#make colormap that is all white to indicate gaps in data
-cmap2 = (mpl.colors.ListedColormap([ '#004488', '#DDAA33']).with_extremes(over='#BB5566', under='#FFFFFF'))
-bounds2 = [0.5,1]
-norm2 = mpl.colors.BoundaryNorm(bounds2, cmap2.N)
-
-fig, axes = plt.subplots(1,2,sharey='row',figsize=[7.5,5],gridspec_kw={'width_ratios': [1,3]},tight_layout=True)
+fig, axes = plt.subplots(1,2,sharey='row',figsize=[7.5,6],gridspec_kw={'width_ratios': [1,3]},tight_layout=True)
 for ax in axes: 
+    j = 0 
     for i in range(nruns):
         #load Rem data
         run = int(runval[i])
@@ -93,32 +76,36 @@ for ax in axes:
         toff2 = var_results.loc[var_results['run']==run,'magoff_2'].values[0]
         toff3 = var_results.loc[var_results['run']==run,'magoff_3'].values[0]
         if toff3 != 0: #3 dynamo periods
-            Rem[(t>tsolid_start)&(t<toff3)&(Rem==0)]=np.average(Rem[(t>tsolid_start)&(t<toff3)])
+            Rem[(t>tsolid_start)&(t<toff3)]=np.average(Rem[(t>tsolid_start)&(t<toff3)])
         elif toff2 !=0: #2 dynamo periods
-            Rem[(t>tsolid_start)&(t<toff2)&(Rem==0)]=np.average(Rem[(t>tsolid_start)&(t<toff2)])
+            Rem[(t>tsolid_start)&(t<toff2)]=np.average(Rem[(t>tsolid_start)&(t<toff2)])
         else: #1 dynamo period
             toff1 = var_results.loc[var_results['run']==run,'magoff_1'].values[0]
-            Rem[(t>tsolid_start)&(t<toff1)&(Rem==0)]=np.average(Rem[(t>tsolid_start)&(t<toff1)])
+            Rem[(t>tsolid_start)&(t<toff1)]=np.average(Rem[(t>tsolid_start)&(t<toff1)])
         
+        if (ytick_lab[j]==''): #don't plot in gaps
+            ax.hlines(yplot[2*j],0.8,210,color='grey',linestyle='dashed',alpha=0.5,linewidth=0.5)
+            j=j+1
+            
         #make the plot
-        ax.pcolormesh(t,yplot[2*i:2*i+2],[Rem,Rem],cmap=cmap,norm=norm)
+        ax.pcolormesh(t,yplot[2*j:2*j+2],[Rem,Rem],cmap=cmap,norm=norm)
+        ax.barh(yplot[2*j+1],t[-1],color='white',height=1)
+    
+        ax.vlines(var_results.loc[var_results['run']==run,'tsolid_start'],yplot[2*j]-0.5,yplot[2*j]+0.5,color='black')
+        j = j+1
         
-        if (ytick_lab[2*i]!='')&(ytick_lab[2*i-1]!='')&(i>0):
-            ax.hlines(yplot[2*i]-1,0.8,210,color='black',linestyle='dashed',linewidth=0.5)
-        ax.pcolormesh(t,yplot[2*i+1:2*i+3],np.zeros([2,len(Rem)]),cmap=cmap2,norm=norm2) #add gaps between variables
-        ax.vlines(var_results.loc[var_results['run']==run,'tsolid_start'],yplot[2*i]-0.5,yplot[2*i]+0.5,color='black')
-         
-    ax.set_xlabel('Time/Ma') 
-    ax.set_ylim([yplot[0]-1,yplot[-1]+0.5])
+    
+    ax.set_ylim([yplot[0]-1,yplot[-1]+0.5]) 
 axes[0].set_xlim([0.8,xmax])
 axes[1].set_xlim([xmax,500])
-axes[0].set_yticks(ytick_val2,ytick_lab2)
-axes[1].set_title(f'>{xmax} Ma')
-axes[0].set_title(f'First {xmax} Ma')
+axes[0].set_yticks(yplot[::2],ytick_lab[:-1])
+axes[0].tick_params(axis='y', labelsize=7) #make y labels smaller
+axes[1].set_title(f'>{xmax} Ma',fontsize=10)
+axes[0].set_title(f'First {xmax} Ma',fontsize=10)
 cax = fig.add_axes([0.87, 0.17, 0.01, 0.3])
 fig.colorbar(mpl.cm.ScalarMappable(cmap=cmap, norm=norm),cax=cax,extend='both',ticks=bounds,spacing='proportional',
              extendfrac=0.5, orientation='vertical', label='Re$_m$')
-
+fig.suptitle('Time after CAI formation / Ma',y=0,fontsize=10)
 
 if save == True:
     plt.savefig(f'../Plots/{savefolder}Rem_bars_full.png',dpi=450,bbox_inches='tight') 
