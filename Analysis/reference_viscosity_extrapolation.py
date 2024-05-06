Correct units

Analysis/reference_viscosity_extrapolation.py
@@ -72,7 +72,7 @@ def pmid_calc(rhom,rhoc,rc,rmid,r):
 r = np.array([100,200,300,400,500])*1e3 #[m]
 rc = r/2
 rmid = 0.75*r
-pmid = pmid_calc(rhom,rhoc,rc,rmid,r)
+pmid = pmid_calc(rhom,rhoc,rc,rmid,r) # [Pa]
 
 #activation energies from Karato and Wu 1993 and Hirth & Kohlstedt 2003
 E = np.array([240,300,325,430,540,570])*1e3 # [J /mol]
@@ -87,9 +87,9 @@ n = len(V)
 
 
 #%% Extrapolation case 1: Mars, Plesa et. al. 2020
-eta0 = 1e21
-pref = 3e9
-Tref = 1600
+eta0 = 1e21 #[Pas]
+pref = 3e9  # [GPa]
+Tref = 1600# [K]
 
 #find spread in values
 peta_ast = np.zeros([l,m,n])
@@ -107,7 +107,7 @@ print(f'From Mars: The maximum asteroid FK reference viscosity is {np.max(peta_f
 
 #%% Extrapolation case 2: Ganymede Ruckriemen 2018 - parameters from Table 1
 eta0 = np.array([1e19,1e22])
-rg = 2634*1e3 #Ganymede radius
+rg = 2634*1e3 #Ganymede radius [m]
 rgc = 1000*1e3 #average core radius [m]
 rgm = 1784*1e3 #Ganymede mantle radius [m]
 
@@ -115,9 +115,10 @@ rgmid = rgc + rgm/2 #mid mantle radius [m]
 rhomg = 3300 #density of silicate layer [kg m^-3] from Ruckriemen 2018
 rhocg = 6072 #density of core [kg m^-3] from Ruckriemen 2018
 rhoi = 1200 # density of ice [kg m^-3] from Ruckriemen 2018
+# Ganymede has an ice layer!
 pref = 2*np.pi*G/3*(rhomg**2*(rgm**2-rgmid**2)+rhoi**2*(rg**2-rgm**2)+2*rhomg*(rhocg-rhomg)*rgc**3*((1/rgmid)-(1/rgm)))\
         -2*np.pi*G/3*(2*rhoi*(rgc**3*rhocg+(rgm**3-rgc**3)*rhomg-rgm**3*rhoi)*((1/rg)-(1/rgm)))
-Tref = 1600
+Tref = 1600 #[K]
 
 #find spread in values
 reta_ast = np.zeros([2,l,m,n])
@@ -135,8 +136,8 @@ print(f'From Ganymede: The minimum asteroid FK reference viscosity is {np.min(re
 print(f'From Ganymede: The maximum asteroid FK reference viscosity is {np.max(reta_fk):0e} Pas')
 
 #%% Extrapolation case 3: Scott & Kohlstedt 2006 experimental pararmeters
-eta0 = 1e14
-Tref = 1523 #but no melt so 1400 may be more appropriate
+eta0 = 1e14 #[Pas]
+Tref = 1523 #but no melt so 1400 may be more appropriate, [K]
 pref = 300*1e6 #confining pressure [Pa]
 
 #find spread in values