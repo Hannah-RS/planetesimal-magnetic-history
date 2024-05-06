0cf1439231173a9f65adc1c678007b4ae653d5d4
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2024-05-06 16:29:02 +0100

    Move hpc shell script and delete unnecessary shell scripts

--- rprocess.sh
+++
@@ -1,9 +0,0 @@
-## Move results from a set of runs to their correct folders ready for extraction
-cd $DATA/Asteroid_model/Results
-mkdir $1 #make directory
-mv params_* $1/ #move results to directory
-cd ../Run_params
-mv auto_params* $1/ #move run params back to their folder
-cd ../.. 
-mkdir Output_$1 #move output files to a folder
-mv arrayJob* Output_$1/  
