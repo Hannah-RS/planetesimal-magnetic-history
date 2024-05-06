25fe106361f084f45ad6161dd561a07bce7512b6
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2024-05-06 16:41:06 +0100

    Remove hpc scripts

--- hpc_scripts/send_run_params.sh
+++
@@ -1,3 +0,0 @@
-#send a folder of run parameters to the hpc
-cd
-scp -r Documents/Code/learning-model/Run_params/$1/ arc-file://data/eart-astroid-evo/exet5460/Asteroid_model/Run_params
--- hpc_scripts/sync.sh
+++
@@ -1,4 +0,0 @@
-# shell script to synchronise code folder with hpc
-cd #change to home directory
-rsync -va --exclude=*/ Documents/Code/learning-model/ arc-file://data/eart-astroid-evo/exet5460/Asteroid_model #copy everything except directories
-rsync -var Documents/Code/learning-model/Templates arc-file://data/eart-astroid-evo/exet5460/Asteroid_model #copy the templates directory 
