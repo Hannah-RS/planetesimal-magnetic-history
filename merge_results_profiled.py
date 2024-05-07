069d43a3edffced190dfbba8ec79c2c64e45eb43
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2024-05-07 09:58:38 +0100

    Updated profiled version of code

--- merge_results_profiled.py
+++ merge_results_profiled.py
@@ -19,19 +19,23 @@ def combine_csv():
     for i in range(num_runs):
         #combine all run params and results
         # by default the merging here will remove unfinished runs which didn't save mag field parameters
-        results = combine_info(folder+subfolder+f'{(i+1)}/','auto_params.csv','run_results.csv',['MAC_onoff.csv','CIA_onoff.csv','comp_onoff.csv','coreconv_onoff.csv'])   
+        results = combine_info(folder+subfolder+f'{(i+1)}/','auto_params.csv','run_results.csv')   
         sucesses = results[results['status']==1]
         nsuc = nsuc + len(sucesses)
-        sucesses.to_csv(f'{folder}all_sucess_info2.csv',index=False,mode='w',header=False)
+        sucesses.to_csv(f'{folder}all_sucess_info.csv',index=False,mode='a',header=False)
         #count number of failed runs and combine parameters for rerun
         run_info = pd.read_csv(folder+subfolder+f'{(i+1)}/auto_params.csv',delimiter=',',skiprows=[1]) 
-        fails = run_info[run_info['status']!=1]
+        fails = run_info[run_info['status']==0]
         nfails = nfails+len(fails)
-        fails.to_csv(f'{folder}fail_params2.csv',index=False,mode='w',header=False)
-    
-    ntot = nfails+nsuc #total number of runs    
+        fails.to_csv(f'{folder}fail_params.csv',index=False,mode='a',header=False)
+        #count number of invalid viscosity profiles
+        inval = run_info[run_info['status']==-1]
+        ninval = ninval+len(inval)
+        inval.to_csv(f'{folder}inval_params.csv',index=False,mode='a',header=False)
+    ntot = nfails+nsuc+ninval #total number of runs    
     print(f'{nsuc} runs were sucessful - {nsuc/ntot*100:.1f}%')
     print(f'{nfails} runs failed - {nfails/ntot*100:.1f}%')
+    print(f'{ninval} runs had invalid viscosity parameters - {ninval/ntot*100:.1f}%')
 
         
 if __name__ == '__main__':
