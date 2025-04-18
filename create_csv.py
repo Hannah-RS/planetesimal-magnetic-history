e4e8fc909cbbdf3edb356cf33abe82fc03614c52
Hannah Sanderson <exet5460@earth.ox.ac.uk>
2025-04-18 10:56:41 +0100

    Update parameter combinations for multivariate runs

--- create_csv.py
+++ create_csv.py
@@ -6,87 +6,95 @@ Script to create auto_params.csv for all possible combinations of parameters
 import pandas as pd
 import numpy as np
  
-folder = 'Run_params/Fullrun2/' #folder to save parameters in   
+folder = 'Run_params/Fullrun/' #folder to save parameters in   
 #for parameter list
 #each variable
 #radius [m]
 nr = 4 #number of points
-minr = 100e3 #min val
-maxr = 400e3 #max val
+minr = 200e3 #min val
+maxr = 500e3 #max val
 r = np.linspace(minr,maxr,nr) 
 
 #rcmf
-nrcmf = 4
-minrcmf = 0.2
+nrcmf = 3
+minrcmf = 0.3
 maxrcmf = 0.5
 rcmf = np.linspace(minrcmf,maxrcmf,nrcmf)
 
 #reference viscosity
-neta0 = 7
-mineta0 = 14 #10^{mineta0}
-maxeta0 = 21 
+neta0 = 5
+mineta0 = 15 #10^{mineta0}
+maxeta0 = 23 
 eta0 = np.logspace(mineta0,maxeta0,neta0)
 
 #beta
 nbeta = 5
-minbeta = 0.005
-maxbeta = 0.08
+minbeta = 0.01
+maxbeta = 0.05
 beta = np.linspace(minbeta,maxbeta,nbeta)
 
 #sulfur content
 nxs = 3
-minxs = 28.5 #will vary by body size later
+minxs = 28.7 #will vary by body size later
 maxxs = 32
 Xs_0 = np.linspace(minxs,maxxs,nxs)
 
-#iron content - specified manually for now
-nfe = 3
-minfe = 0
-maxfe = 1e-7
-Fe0 = np.array([0,1e-8,1e-7])
+#melt weakening exponent
+nalphan = 5
+minalphan = 25
+maxalphan = 45
+alpha_n = np.linspace(minalphan,maxalphan,nalphan) 
 
-#liquid viscosity
-netal = 3
-minetal = 1 #log10(etal)
-maxetal = 3 #log10(etal)
-etal = np.logspace(minetal,maxetal,3)
+#core radius fraction
+nrc = 5
+minrc = 0.1
+maxrc = 0.9
+rc = np.linspace(minrc,maxrc,nrc)
 
-#diffusion vs dislocation creep
-#alpha_n = np.array([25,30]) #diffusion vs (diffusion+dislocation) creep (S&C)
-
+#water content
+nxw = 4
+xw = np.array([0,0.02,0.05,0.07]) #wt %
 
 #parameters fixed in this combination
+accrete = False #start at differentiation
 default = 'vary' #viscosity profile will change
-t_acc_m = 0.8 #accretion time [Myr]
+t_start_m = 1 #accretion time [Myr]
 t_end_m = 1000 #maximum end time of simulation [Myr]
 w = 5 #width of linear region [K]
-#etal = 100 #liquid viscosity [Pas]
+etal = 100 #liquid viscosity [Pas]
+Fe0 = 1e-8 #initial 60Fe/56Fe
 dr = 500 # grid space [m]
 dt = 0.075 #timestep [fraction of core conductive timestep]
-alpha_n = 25 #melt weakening, diffusion creep
+icfrac = 1 #core solidification endmember
 
 # create dictionaries and write to csv
 run = 1
 csv_num = 1
-run_info = pd.DataFrame(columns=['run','r','default','rcmf','eta0','beta','w','etal','alpha_n','Xs_0','Fe0','t_acc_m','t_end_m','dr','dt','status']) #create columns of dataframe
-unit_row = ['','m','','','Pas','K^-1','K','Pas','','wt %','60Fe/56Fe','Myr','Myr','m','t_cond_core',''] #first row is units
+run_info = pd.DataFrame(columns=['run','r','rcr','default','rcmf','eta0','beta','w','etal','alpha_n','Xs_0','Fe0','t_start_m','t_end_m','dr','dt','icfrac','xwater','accrete','status']) #create columns of dataframe
+unit_row = ['','m','','','','Pas','K^-1','K','Pas','','wt %','60Fe/56Fe','Myr','Myr','m','t_cond_core','','wt %','',''] #first row is units
 run_info.loc[len(run_info)] = unit_row
-for n, feval in enumerate(Fe0):
+for n, xwval in enumerate(xw):
     for j, rcmfval in enumerate(rcmf):
         for k, eta0val in enumerate(eta0):
             for l, betaval in enumerate(beta):
-                for p, etalval in enumerate(etal):
-                    if run > 1:
-                        #skip this step on the first step down
-                        run_info.to_csv(f'{folder}auto_params_{csv_num}.csv',index=False)
-                        csv_num = csv_num + 1 #start making a new csv
-                        run_info = pd.DataFrame(columns=['run','r','default','rcmf','eta0','beta','w','etal','alpha_n','Xs_0','Fe0','t_acc_m','t_end_m','dr','dt','status']) #create columns of dataframe
-                        unit_row = ['','m','','','Pas','K^-1','K','Pas','','wt %','60Fe/56Fe','Myr','Myr','m','t_cond_core',''] #first row is units
-                        run_info.loc[len(run_info)] = unit_row
-                    for m, xsval in enumerate(Xs_0):
-                        for i, rval in enumerate(r): #run r as last so all sets of files take similar time
-                            run_info = pd.concat([run_info, pd.DataFrame({"run":[run],"r":[rval],"default":[default],"rcmf":[rcmfval],"eta0":[eta0val],"beta":[betaval],"w":[w],"etal":[etalval],"alpha_n":[alpha_n],"Xs_0":[xsval], "Fe0":[feval], "t_acc_m":[t_acc_m], "t_end_m":[t_end_m], "dr":[dr],"dt":[dt],"status":""})],ignore_index=True)
-                            run = run+1
+                for p, rcval in enumerate(rc):
+                    for q, alphanval in enumerate(alpha_n):
+                        if run > 1:
+                            #skip this step on the first step down
+                            run_info.to_csv(f'{folder}auto_params_{csv_num}.csv',index=False)
+                            csv_num = csv_num + 1 #start making a new csv
+                            run_info = pd.DataFrame(columns=['run','r','rcr','default','rcmf','eta0','beta','w','etal','alpha_n','Xs_0','Fe0','t_start_m',
+                                                             't_end_m','dr','dt','icfrac','xwater','accrete','status']) #create columns of dataframe
+                            unit_row = ['','m','','','','Pas','K^-1','K','Pas','','wt %','60Fe/56Fe','Myr','Myr','m','t_cond_core','','wt %','',''] #first row is units
+                            run_info.loc[len(run_info)] = unit_row
+                        for m, xsval in enumerate(Xs_0):
+                            for i, rval in enumerate(r): #run r as last so all sets of files take similar time
+                                run_info = pd.concat([run_info, pd.DataFrame({"run":[run],"r":[rval],"rcr":[rcval],"default":[default],
+                                                                              "rcmf":[rcmfval],"eta0":[eta0val],"beta":[betaval],"w":[w],"etal":[etal],
+                                                                              "alpha_n":[alphanval],"Xs_0":[xsval], "Fe0":[Fe0], "t_start_m":[t_start_m],
+                                                                                "t_end_m":[t_end_m], "dr":[dr],"dt":[dt],"icfrac":[icfrac],"xwater":[xwval],
+                                                                                "accrete":[accrete],"status":""})],ignore_index=True)
+                                run = run+1
 
 #call once more at the end for the final csv                        
 run_info.to_csv(f'{folder}auto_params_{csv_num}.csv',index=False)                        
