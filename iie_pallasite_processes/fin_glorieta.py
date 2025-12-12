d0a1c5fa90e9af5d4705edd65d25901ae1edbfcd
Hannah-RS <92574098+Hannah-RS@users.noreply.github.com>
2025-12-12 13:38:44 +0100

    Workflow for determining magnetic properties of Finmarken and Glorieta Mountain

---
+++ iie_pallasite_processes/fin_glorieta.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Predict whether Finmarken and Glorieta Mountain will record a dynamo based on sucessful model runs
+"""
+#%% Imports
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append('../')
+Myr = 1e6*365*24*3600 #seconds in a million years
+from pallasite_dynamo_functions import dynamo_status
+from pallasite_cooling_functions import find_temp_depth, find_abs_depth
+from average_B import average_B_rem
+
+# Load data
+folder = sys.argv[1]
+subfolder = sys.argv[2]
+mdata = pd.read_csv(f'../Results/{folder}/pallasite_sucess_info.csv',skiprows=[1]) #sucessful model params
+mdata = mdata[mdata['run']<=12]
+edata = {'cr_yang_low': np.array([2.2, 17.5]),
+         'cr_yang_up': np.array([2.8, 19.9]),} #[K/Myr] Yang et. al. 2010 cooling rates at 925K
+Remc = 10 #critical magnetic Reynolds number
+Xs_eutectic=33 #eutectic composition
+#create dataframe to store output
+pdata = pd.DataFrame(columns=['run','gm_dlow','gm_dup','f_dlow','f_dup','gm_tlow','gm_tup','f_tlow','f_tup','gm_mag','f_mag','gm_core_solid','f_core_solid'])
+#%% Loop over runs
+i = 0 # index for saving to dataframe
+for run in mdata['run']:
+    #navigate to correct subfolder
+    folder_num = int(run*7500/90000) +1
+    subfolder = f'params_{folder_num}'
+    mout = pd.read_csv(f'../Results/{folder}/{subfolder}/run_results.csv',skiprows=[1]) #model outputs
+    run = int(run)
+    #load data
+    npzfile = np.load(f'../Results/{folder}/{subfolder}/run_{run}_B.npz')
+    B = npzfile['B']
+    Rem = npzfile['Rem']
+    Xs = npzfile['Xs']
+    t = npzfile['t']/Myr
+    temp = npzfile['T_profile']
+    r = mdata.loc[mdata['run'] == run, 'r'].values[0]
+    rcr = mdata.loc[mdata['run'] == run, 'rcr'].values[0]
+    Xs0 = mdata.loc[mdata['run'] == run, 'Xs_0'].values[0]
+    eta0 = mdata.loc[mdata['run'] == run, 'eta0'].values[0]
+    dr = mdata.loc[mdata['run'] == run, 'dr'].values[0]
+    dt = t[1] - t[0] #time step in data [Myr]
+    tsolid_start = mout.loc[mout['run'] == run, 'tsolid_start'].values[0]
+    #average B and Rem
+    if Xs0!=Xs_eutectic: #if the core doesn't start at the eutectic composition
+        B, Rem = average_B_rem(B, Rem, t, Xs, Xs_eutectic, tsolid_start)
+    #calculate radial grid and truncate Tprofile
+    n_cells = int(r/dr) +1 #number of cells needed to span the body including one at the centre
+    i_core = round((n_cells-3)*rcr) +1 #index of CMB
+    rprof = np.arange(i_core*dr,r+dr,dr)/1e3
+    temp = temp[:,i_core:] #restrict to mantle cells
+    #calculate dTdt
+    tempdt = -np.gradient(temp,dt,axis=0) #cooling rate [K/Myr]
+    
+    #find contour for Yang et. al. 2010 data, find depths where cooling rates match
+    rind = find_temp_depth(925,edata['cr_yang_low'],edata['cr_yang_up'], t, temp, tempdt) 
+    #find depths of each sample
+    depth, time = find_abs_depth(rind,rprof,t,temp,925)
+
+    #find out if the dynamo is on/off at 593K for each sample
+    Bmodel, c = dynamo_status(rind,t,temp,Rem,Remc,B,tsolid_start)
+    
+
+    #save to file
+    pdata.loc[i] = [run, depth[0, 0], depth[0, 1], depth[1, 0], depth[1, 1], time[0,0], time[0,1], time[1,0], time[1,1], Bmodel[0], Bmodel[1],c[0],c[1]]
+    i += 1
+
+# merge with overall results
+mdata['run'] = mdata['run'].astype(int) #convert to int
+sucesses = pd.merge(mdata, pdata, on="run") #join together on matching runs
+sucesses.to_csv(f'../Results/{folder}/gmf_sucess_info.csv',index=False,mode='a',header=False)
